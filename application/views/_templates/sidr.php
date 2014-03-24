58ea78a5974964309e7bc57f5e41b1572fd060e2
Gizmmo <travisscott301@gmail.com>
2014-03-24 07:33:57 -0500

    Updated Entry

--- application/views/_templates/sidr.php
+++ application/views/_templates/sidr.php
@@ -2,9 +2,9 @@
   <!-- Your content -->
   <ul>
   	<?php 
-  		$user_id = 1;
-        $userProjects_model = $this->loadModel('UserProjectsModel');
-        $userProjects = $userProjects_model->getUserOwnRecentProjects($user_id);
+  		  $user_id = 1;
+        $side_model = $this->loadModel('UserProjectsModel');
+        $userProjects = $side_model->getUserOwnRecentProjects($user_id);
   	?>
   	<?php foreach ($userProjects as $project) { ?>
   		<li><a href="<?php echo URL; ?>diary/projectdiary/<?php echo $project->id ?>"><?php  echo $project->name ?></a></li>
